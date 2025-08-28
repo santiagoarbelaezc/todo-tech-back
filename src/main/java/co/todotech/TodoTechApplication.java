fd9acaa11509506bdcb5cb5870bc4ba80993cbbd
FedeAlvarezzz <federico.alvarezm@uqvirtual.edu.co>
2025-08-27 23:26:03 -0500

    Creacion de proyecto TodoTech (Backend)

---
+++ src/main/java/co/todotech/TodoTechApplication.java
@@ -0,0 +1,13 @@
+package co.todotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoTechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/todotech/TodoTechApplicationTests.java
@@ -0,0 +1,13 @@
+package co.todotech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoTechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
